747eb09cf11529d1454b8030575a76f26ebf8401
App App <app@myserver1.tpsrvr1.d10.internal.cloudapp.net>
2015-08-04 23:43:34 +0000

    added deploy script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('hello world and dearest natives.');
+    res.send('Hello world and dearest natives.');
 });
 
 app.listen(process.env.POR || 5000);
