8904eb85f373b1f632318185c07ef57c9dfc989a
test user <fake@fake.com>
2015-08-04 12:48:43 -0700

    fixed both program and test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('hello world and dearest locals');
+    res.send('hello world and dearest locals.');
 });
 
 app.listen(process.env.POR || 5000);
